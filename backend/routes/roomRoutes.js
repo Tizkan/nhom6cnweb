7f9ce5d9dd23872ab95f1397a36e01e44b5d6beb
Lê Phạm Quốc Thịnh <thinhvn26@gmail.com>
2026-06-06 22:06:26 +0700

    them phong, sua phong, xoa phong

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const roomController = require("../controllers/roomController");
 
-router.get("/", roomController.getRooms);
+router.get("/",        roomController.getRooms);
+router.get("/:id",     roomController.getRoomById);
+router.post("/",       roomController.createRoom);
+router.put("/:id",     roomController.updateRoom);
+router.delete("/:id",  roomController.deleteRoom);
 
 module.exports = router;
